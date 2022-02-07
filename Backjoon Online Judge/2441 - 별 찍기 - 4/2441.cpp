c6e49ff9b658b039a32800787e350053a1519fc6
SeoriNn <train1312@naver.com>
2022-02-07 21:00:05 +0900

    update

---
+++ Backjoon Online Judge/2441 - 별 찍기 - 4/2441.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void) {
+	int n = 0;
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++)
+			std::cout << " ";
+		for (int j = i; j < n; j++)
+			std::cout << "*";
+		std::cout << "\n";
+	}
+
+	return 0;
+}
